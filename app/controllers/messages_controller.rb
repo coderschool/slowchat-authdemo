5de5156e38795918a8f9b6e3492920540ddc263d
Charles Lee <chug2k@gmail.com>
2015-11-23 19:19:58 +0700

    basic message display

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,8 +1,8 @@
 class MessagesController < ApplicationController
 
   before_action :require_login
-  
+
   def index
-        
+    @messages = Message.where(recipient: current_user)      
   end
 end
